38872a2ff3cdf39485e00c91c6d71277e2f34132
thuliumkyg <1299338908@qq.com>
2021-11-14 16:07:25 +0800

    study Reactor router

---
+++ src/main/java/com/example/springwebflux/service/ProductHandler.java
@@ -0,0 +1,38 @@
+package com.example.springwebflux.service;
+
+
+import com.example.springwebflux.entity.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+@Component
+public class ProductHandler {
+
+    @Autowired
+    private ProductService productService;
+
+    public Mono<ServerResponse> getProducts(ServerRequest request) {
+        return ServerResponse.ok().body(this.productService.getProducts(), Product.class);
+    }
+
+    public Mono<ServerResponse> getProductById(ServerRequest request) {
+        String id = request.pathVariable("id");
+
+        return ServerResponse.ok().body(this.productService.getProductsById(id), Product.class);
+    }
+
+    public Mono<ServerResponse> createProduct(ServerRequest request) {
+        Mono<Product> productMono= request.bodyToMono(Product.class);
+
+        return ServerResponse.ok().body(this.productService.createOrUpdateProduct(productMono), Product.class);
+    }
+
+    public Mono<ServerResponse> deleteProduct(ServerRequest request) {
+        String id = request.pathVariable("id");
+
+        return ServerResponse.ok().body(this.productService.deleteProduct(id), Product.class);
+    }
+}
