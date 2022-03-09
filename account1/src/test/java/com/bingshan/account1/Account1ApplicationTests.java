02a8af0bb1725cd134e9360617d1411104484ab2
thuliumkyg <1299338908@qq.com>
2022-03-09 22:32:13 +0800

    Spring Cloud 构建响应式为服务架构 study zipkin

---
+++ account1/src/test/java/com/bingshan/account1/Account1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bingshan.account1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Account1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
