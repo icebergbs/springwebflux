fc0085961549b90b8f37603fa229a556cebb2c39
geky <geky@esupplychain.com>
2022-03-25 21:08:06 +0800

    Reactor3

---
+++ src/main/java/com/example/springwebflux/controller/MyEventListener.java
@@ -0,0 +1,14 @@
+package com.example.springwebflux.controller;
+
+import java.util.List;
+
+/**
+ *
+ *
+ * @author  bingshan
+ * @date 2022/3/25 21:01
+ */
+public interface MyEventListener<T> {
+    void onDataChunk(List<T> chunk);
+    void processComplete();
+}
--- src/main/java/com/example/springwebflux/controller/Reactor3_1_Core.java
+++ src/main/java/com/example/springwebflux/controller/Reactor3_1_Core.java
@@ -4,6 +4,7 @@ import reactor.core.publisher.Flux;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  *
@@ -35,5 +36,70 @@ public class Reactor3_1_Core {
                 () -> {System.out.println("Done");},
                 s -> ss.request(10));
         ints.subscribe(ss);
+
+
+        System.out.println("-------Generate------");
+
+        //4.4 可编程式地创建一个序列
+        //如何通过定义相对应的事件（onNext、onError`和`onComplete） 创建一个 Flux 或 Mono。
+        // 所有这些方法都通过 API 来触发我们叫做 sink（池） 的事件。
+
+
+        //sink 的类型
+        // 1.Generate
+        //这是一种 同步地， 逐个地 产生值的方法，
+        // 意味着 sink 是一个 SynchronousSink 而且其 next() 方法在每次回调的时候最多只能被调用一次
+        Flux<String> flux = Flux.generate(
+                () -> 0,
+                (state, sink) -> {
+                    sink.next("3 x" + state + " = " + 3*state);
+                    if (state == 10)
+                        sink.complete();
+                    return state + 1;
+                }
+        );
+        flux.subscribe(System.out::println);
+
+        System.out.println("-------mutable------");
+        //可变类型的状态变量
+        Flux<String> flux1 = Flux.generate(
+                AtomicLong::new,
+                (state, sink) -> {
+                    long i = state.getAndIncrement();
+                    sink.next("3 x" + i + " = " + 3*i);
+                    if (i == 10)
+                        sink.complete();
+                    return state;
+                }
+        );
+        flux1.subscribe(System.out::println);
+
+        //2   Create
+        //作为一个更高级的创建 Flux 的方式， create 方法的生成方式既可以是同步， 也可以是异步的，并且还可以每次发出多个元素。
+        //该方法用到了 FluxSink.   与 generate 不同的是，create 不需要状态值，
+        // 另一方面，它可以在回调中触发 多个事件（即使是在未来的某个时间）。
+
+        //create 有个好处就是可以将现有的 API 转为响应式，比如监听器的异步方法。
+        Flux<String> bridge = Flux.create(sink -> {
+            myEventProcessor.register(
+                    new MyEventListener<String>() {
+                        @Override
+                        public void onDataChunk(List<String> chunk) {
+                            for (String s : chunk) {
+                                sink.next(s);
+                            }
+                        }
+
+                        @Override
+                        public void processComplete() {
+                            sink.complete();
+                        }
+                    }
+            );
+        });
+
+
+
+
     }
 }
